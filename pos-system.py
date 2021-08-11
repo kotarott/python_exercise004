5ff82084bcf415e3b41e6d62b2f0ef86178126a7
kotarott <kg221kg@gmail.com>
2021-08-11 23:26:43 +0900

    csv作成機能

--- pos-system.py
+++ pos-system.py
@@ -1,5 +1,6 @@
 import os
 import pandas as pd
+import datetime
 
 
 ### 商品クラス
@@ -15,9 +16,11 @@ class Item:
 ### オーダークラス
 class Order:
     def __init__(self,item_master):
-        self.item_order_list=[]
-        self.item_master=item_master
-        self.item_sum=0
+        self.item_order_list = []
+        self.item_master = item_master
+        self.item_sum = 0
+        self.payment = 0
+        self.orders = []
     
     def add_item_order(self,item_code, item_num):
         self.item_order_list.append([item_code, item_num])
@@ -32,6 +35,7 @@ class Order:
                         + ",注文数:{}".format(item[1])
                         + ",注文金額:{}".format(master.get_price() * item[1])
                     )
+                    self.orders.append([item[0], master.item_name, master.get_price(), item[1], master.get_price() * item[1]])
     
     def sum(self):
         for item in self.item_order_list:
@@ -41,11 +45,9 @@ class Order:
 
     def get_oturi(self, payment):
         self.sum()
-        oturi = payment - self.item_sum
-        if oturi < 0:
-            print("不足金額:{}".format(-1 * oturi))
-        else:
-            print("お釣り:{}".format(oturi))
+        self.payment = payment
+        return self.payment - self.item_sum
+        
 
 ### メイン処理
 def main():
@@ -59,26 +61,33 @@ def main():
     item_master = input_master(path)
 
     # オーダー登録
-    order=Order(item_master)
+    order = Order(item_master)
     order.add_item_order("001", 2)
     order.add_item_order("002", 1)
     order.add_item_order("003", 4)
     
     # オーダー追加
-    add_item(order)
+    while input("注文を終了しますか？（はい:y,いいえ:n）\n>>> ") != "y":
+        add_item(order)
 
     # オーダー表示
     order.view_item_list()
     
-    payment = input("支払金額を入力してください。\n>>> ")
-    order.get_oturi(int(payment))
+    # 支払いとお釣り
+    result = order.get_oturi(int(input("\n支払金額を入力してください。\n>>> ")))
+    if result < 0:
+        print("不足金額:{}".format(-1 * result))
+    else:
+        print("お釣り:{}".format(result))
 
+    # csvアウトプット
+    output(order)
     
 
-def add_item(cls):
+def add_item(order):
     input_code = input("商品コードを入力してください。\n>>> ")
     input_num = input("個数を入力してください。\n>>> ")
-    cls.add_item_order(input_code, int(input_num))
+    order.add_item_order(input_code, int(input_num))
 
 def input_master(path):
     if os.path.exists(path) != True:
@@ -92,8 +101,16 @@ def input_master(path):
         item_master.append(Item(row[0], row[1], row[2]))
     
     return item_master
-        
 
+def output(order):
+    now = datetime.datetime.now().strftime("%Y%m%d")
+    with open(now + ".csv", "w", encoding="UTF-8") as f:
+        f.write(",".join(["商品コード", "商品名", "価格", "注文数", "合計金額"]))
+        for item in order.orders:
+            f.write("\n")
+            f.write(",".join(map(str, item)))
+        f.write("\n\n")
+        f.write(",".join(["合計金額:", str(order.item_sum), "支払額:", str(order.payment), "お釣り:", str(order.payment - order.item_sum)]))
     
 if __name__ == "__main__":
     main()
